0ed7223a8ea99276e4f210c0539f8c2567d83f27
dvanzhula <dvanzhula@gmail.com>
2016-10-20 00:34:51 +0300

    added new file testdata.csv, added new dependency JUnitParams in  pom.xml, added new test class MathFuncTest

---
+++ src/test/java/com/dvanzhula/app/classwork/lesson6/MathFuncTest.java
@@ -0,0 +1,24 @@
+package com.dvanzhula.app.classwork.lesson6;
+
+import com.dvanzhula.app.classwork.lesson4.MathFunctions;
+import junitparams.FileParameters;
+import junitparams.JUnitParamsRunner;
+import junitparams.mappers.CsvWithHeaderMapper;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by dmitry on 19.10.16.
+ */
+@RunWith(JUnitParamsRunner.class)
+public class MathFuncTest {
+
+    @Test
+    @FileParameters(value = "/home/dmitry/Projects/javacore/src/test/resources/testdata.csv",
+            mapper = CsvWithHeaderMapper.class)
+    public void test_add(int argA, int argB, int expOut) {
+        MathFunctions mathFunc = new MathFunctions();
+        Assert.assertEquals(expOut, mathFunc.multiply(argA, argB));
+    }
+}
