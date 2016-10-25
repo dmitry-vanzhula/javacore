e1ffe2adc84f001a95a31679535b1b4cb6cfb65f
dvanzhula <dvanzhula@gmail.com>
2016-10-25 18:38:41 +0300

    Added new methods and modified old in lesson 7 homework

--- src/main/java/com/dvanzhula/runners/homework/Lesson7LoopsArraysRunner.java
+++ src/main/java/com/dvanzhula/runners/homework/Lesson7LoopsArraysRunner.java
@@ -6,66 +6,124 @@ package com.dvanzhula.runners.homework;
 public class Lesson7LoopsArraysRunner {
     public static void main(String[] args) {
 
-        Lesson7LoopsArraysRunner.showArrayOfEvenNumbers();
-        Lesson7LoopsArraysRunner.showArrayOfOddNumbers();
-        Lesson7LoopsArraysRunner.showRandomArrayNumbers();
+        Lesson7LoopsArraysRunner.showAnArrayOfMultidimensionalNumbers2();
+
+
     }
 
-    public static void showArrayOfEvenNumbers() {
-        int[] arrayOfEvenNumbers = new int[11];
-        for (int i = 1; i < arrayOfEvenNumbers.length; i++) {
-            arrayOfEvenNumbers[i] = i * 2;
+    public static void showAnArrayOfEvenNumbers() {
+
+        int[] array = new int[11];
+        for (int i = 1; i < array.length; i++) {
+            array[i] = i * 2;
         }
-        for (int i = 1; i < arrayOfEvenNumbers.length; i++) {
-            System.out.println(arrayOfEvenNumbers[i]); // массив в столбик
-        }
-
-        System.out.println(); // пробел между выводом текста на экран
-
-        for (int i = 1; i < arrayOfEvenNumbers.length; i++) {
-            System.out.print(arrayOfEvenNumbers[i] + " "); // массив в строку
-        }
-    }
-
-    public static void showArrayOfOddNumbers() {
-
-        int[] arrayOfOddNumbers = new int[50];
-        for (int i = 0; i < arrayOfOddNumbers.length; i++) {
-            arrayOfOddNumbers[i] = i * 2 + 1;
+        for (int i = 1; i < array.length; i++) {
+            System.out.println(array[i]); // массив в столбик
         }
 
-        for (int i = 0; i < arrayOfOddNumbers.length; i++) {
-            System.out.println(arrayOfOddNumbers[i]); // массив в столбик
-        }
+        System.out.println("----------------------"); // пробел между выводом текста на экран
 
-        System.out.println(); // пробел между выводом текста на экран
-
-        for (int i = 49; i >= 0; i--) {
-            System.out.print(arrayOfOddNumbers[i] + " "); // массив в строку
+        for (int i = 1; i < array.length; i++) {
+            System.out.print(array[i] + " "); // массив в строку
         }
     }
 
-    public static void showRandomArrayNumbers() {
+    public static void showAnArrayOfOddNumbers() {
 
-        int evenNumbers = 0;
-
-        int[] randomArrayOfNumbers = new int[16];
-        for (int i = 0; i < randomArrayOfNumbers.length; i++) {
-            randomArrayOfNumbers[i] = (int) (Math.random() * 10);
+        int[] array = new int[50];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i * 2 + 1;
         }
-
-        for (int i = 0; i < randomArrayOfNumbers.length; i++) {
-            System.out.println(randomArrayOfNumbers[i]);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]); // массив в столбик
         }
         System.out.println("----------------------"); // пробел между выводом текста на экран
 
-        for (int i = 0; i < randomArrayOfNumbers.length; i++) {
-            if (randomArrayOfNumbers[i] % 2 == 0) {
+        for (int i = 49; i >= 0; i--) {
+            System.out.print(array[i] + " "); // массив в строку
+        }
+    }
+
+    public static void showAnArrayOfNumbers() {
+
+        int evenNumbers = 0;
+
+        int[] array = new int[16];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10); // генерация случайных чисел
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]); // массив в столбик
+        }
+        System.out.println("----------------------"); // пробел между выводом текста на экран
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
                 evenNumbers++;
             }
         }
-        System.out.println(evenNumbers);
+        System.out.println("There are " + evenNumbers + " even numbers"); // вывести на экран количество четных чисел
+    }
+
+    public static void showAnArrayOfNumbers2() {
+
+        int[] array = new int[16];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 1000); // генерация случайных чисел
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]); // массив в столбик
+        }
+
+        System.out.println("----------------------"); // пробел между выводом текста на экран
+
+        int min = array[0]; // пусть минимум у нас будет 1-элемент в массиве
+        int max = array[0]; // пусть максимум у нас будет 1-элемент в массиве
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        System.out.println("Minimum number is " + min);
+        System.out.println("Maximum number is " + max);
+    }
+
+    public static void showAnArrayOfMultidimensionalNumbers() {
+
+        int[][] array = new int[8][5];
+        for (int i = 0; i < array.length; i++) {
+            for (int k = 0; k < array[i].length; k++) {
+                array[i][k] = (int) (Math.random() * 100);
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            for (int k = 0; k < array[i].length; k++) {
+                System.out.println(array[i][k]);
+            }
+        }
+    }
+
+    public static void showAnArrayOfMultidimensionalNumbers2() {
+
+        int[][] array = new int[8][5];
+        for (int i = 0; i < array.length; i++) {
+            for (int k = 0; k < array[i].length; k++) {
+                array[i][k] = (int) (Math.random() * 1000);
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf("%d%s", i + 1, ":");
+            for (int k = 0; k < array[i].length; k++) {
+                System.out.printf("%5d", array[i][k]);
+            }
+            System.out.println("");
+        }
+
     }
 }
-
-
