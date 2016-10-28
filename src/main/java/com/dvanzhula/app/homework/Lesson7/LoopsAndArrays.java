5dd08e8576b54451f79fef28ca2ce64d9ad780b5
dvanzhula <dvanzhula@gmail.com>
2016-10-29 00:41:55 +0300

    all methods in lesson 7 have been changed

--- src/main/java/com/dvanzhula/app/homework/Lesson7/LoopsAndArrays.java
+++ src/main/java/com/dvanzhula/app/homework/Lesson7/LoopsAndArrays.java
@@ -1,18 +1,47 @@
 package com.dvanzhula.app.homework.Lesson7;
 
 
+import java.util.Arrays;
+
 /**
  * Created by Dmitriy on 10/27/2016.
  */
 public class LoopsAndArrays {
 
-    public static int[] showAnArrayOfEvenNumbers(int arrayLength) {
-
+    public static int[] createArray(int arrayLength) {
         int[] array = new int[arrayLength];
+        return array;
+    }
+
+    public static int[][] createMultidimensionalArray(int arrayLength, int arrayLength2) {
+        int[][] array = new int[arrayLength][arrayLength2];
+        return array;
+    }
+
+    public static int[] fillAnArrayWithRandomNumb(int arrayLength, int min, int max) {
+
+        int[] array = LoopsAndArrays.createArray(arrayLength);
+        for (int i = 0; i < arrayLength; i++) {
+            array[i] = generatorOfRandomNumbers(min, max);
+        }
+        return array;
+    }
+
+    public static int[][] fillMultidimensionalArrayWithRandomNumb(int arrayLength, int arrayLength2, int min, int max) {
+        int[][] array = LoopsAndArrays.createMultidimensionalArray(arrayLength, arrayLength2);
+        for (int i = 0; i < arrayLength; i++) {
+            for (int k = 0; k < arrayLength2; k++) {
+                array[i][k] = generatorOfRandomNumbers(min, max);
+            }
+        }
+        return array;
+    }
+
+    public static int[] showAnArrayOfEvenNumbers(int arrayLength) {
+        int[] array = LoopsAndArrays.createArray(arrayLength);
         for (int i = 1; i < arrayLength; i++) {
             array[i] = i * 2;
             System.out.print(array[i] + " ");
-            //System.out.println(Arrays.toString(array));
         }
         System.out.println(" ");
         System.out.println(" ");
@@ -20,7 +49,7 @@ public class LoopsAndArrays {
     }
 
     public static int[] showAnArrayOfOddNumbers(int arrayLength) {
-        int[] array = new int[arrayLength];
+        int[] array = LoopsAndArrays.createArray(arrayLength);
         for (int i = 1; i < arrayLength; i++) {
             array[i] = i * 2 + 1;
             System.out.print(array[i] + " ");
@@ -31,91 +60,65 @@ public class LoopsAndArrays {
     }
 
     public static int generatorOfRandomNumbers(int min, int max) {
-        int result = min + (int)(Math.random() * ((max - min) + 1));
+        int result = min + (int) (Math.random() * ((max - min) + 1));
         return result;
     }
 
-    public static int showAnArrayOfRandomNumbersAndCalculateHowManyEven(int arrayLength, int min, int max) {
+    public static int showHowManyEvenNumbers(int arrayLength, int min, int max) {
         int evenNumbers = 0;
-
-        int[] array = new int[arrayLength];
-        for (int i = 0; i < arrayLength; i++) {
-            array[i] = generatorOfRandomNumbers(min,max);
-            System.out.print(array[i] + " ");
-        }
-
+        int[] array = LoopsAndArrays.fillAnArrayWithRandomNumb(arrayLength, min, max);
         for (int i = 0; i < arrayLength; i++) {
             if (array[i] % 2 == 0) {
                 evenNumbers++;
             }
         }
-        System.out.println("  ");
         System.out.print("Even numbers are " + evenNumbers + " ");
-        System.out.println("  ");
+        System.out.println(" ");
+        System.out.println(Arrays.toString(array));
         System.out.println("  ");
         return evenNumbers;
     }
 
-    public static int showAnArrayOfRandomNumbersAndDefineMinNumber(int arrayLength, int min, int max) {
-        int[] array = new int[arrayLength];
-        for (int i = 0; i < arrayLength; i++) {
-            array[i] = generatorOfRandomNumbers(min,max);
-            System.out.print(array[i] + " ");
-        }
-
+    public static int showMinNumber(int arrayLength, int min, int max) {
+        int[] array = LoopsAndArrays.fillAnArrayWithRandomNumb(arrayLength, min, max);
         int minimum = array[0]; // пусть минимум у нас будет 1-элемент в массиве
-
         for (int i = 0; i < arrayLength; i++) {
             if (array[i] < minimum) {
                 minimum = array[i];
             }
         }
-        System.out.println("  ");
         System.out.println(" Minimum number is " + " " + minimum);
         System.out.println("  ");
+        System.out.println(Arrays.toString(array));
         System.out.println("  ");
         return minimum;
     }
 
-    public static int showAnArrayOfRandomNumbersAndDefineMaxNumber(int arrayLength, int min, int max) {
-        int[] array = new int[arrayLength];
-        for (int i = 0; i < arrayLength; i++) {
-            array[i] = generatorOfRandomNumbers(min,max);
-            System.out.print(array[i] + " ");
-        }
-
+    public static int showMaxNumber(int arrayLength, int min, int max) {
+        int[] array = LoopsAndArrays.fillAnArrayWithRandomNumb(arrayLength, min, max);
         int maximum = array[0]; // пусть максимум у нас будет 1-элемент в массиве
-
         for (int i = 0; i < arrayLength; i++) {
             if (array[i] > maximum) {
                 maximum = array[i];
             }
         }
-        System.out.println("  ");
         System.out.println(" Maximum number is " + " " + maximum);
         System.out.println("  ");
+        System.out.println(Arrays.toString(array));
         System.out.println("  ");
         return maximum;
     }
 
-    public static int[][] showMultidimensionalArrayOfRandomNumbers(int arrayLength, int arrayLength2, int min, int max){
-        int[][] array = new int[arrayLength][arrayLength2];
+    public static int[][] showMultidimensionalArrayOfRandomNumbers(int arrayLength, int arrayLength2, int min, int max) {
+        int[][] array = LoopsAndArrays.fillMultidimensionalArrayWithRandomNumb(arrayLength, arrayLength2, min, max);
         for (int i = 0; i < arrayLength; i++) {
             for (int k = 0; k < arrayLength2; k++) {
                 array[i][k] = generatorOfRandomNumbers(min, max);
             }
         }
-        for (int i = 0; i < arrayLength; i++) {
-            for (int k = 0; k < arrayLength2; k++) {
-               System.out.print(array[i][k] + " ");  //  массив в строку
-            }
-        }
-        //System.out.println(Arrays.deepToString(array));
+        System.out.println(Arrays.deepToString(array));
         System.out.println("  ");
         System.out.println("  ");
         return array;
     }
-
 }
-
-
--- src/main/java/com/dvanzhula/runners/homework/Lesson7LoopsArraysRunner.java
+++ src/main/java/com/dvanzhula/runners/homework/Lesson7LoopsArraysRunner.java
@@ -13,12 +13,13 @@ public class Lesson7LoopsArraysRunner {
         while (true) {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Enter a number 1-7:");
-            System.out.println("Number 1 - Task a");
-            System.out.println("Number 2 - Task b");
-            System.out.println("Number 3 - Task c");
-            System.out.println("Number 4 - Task d");
-            System.out.println("Number 5 - Task e");
-            System.out.println("Number 6 - Task g");
+            System.out.println(" ");
+            System.out.println("Number 1 - Show all even numbers in an array");
+            System.out.println("Number 2 - Show all odd numbers in an array");
+            System.out.println("Number 3 - Count the number of even numbers in an array");
+            System.out.println("Number 4 - Determine the minimum number of array");
+            System.out.println("Number 5 - Determine the maximum number of array");
+            System.out.println("Number 6 - Show multidimensional array");
             System.out.println("Number 7 - Exit");
             try {
                 int arrayLength;
@@ -28,50 +29,50 @@ public class Lesson7LoopsArraysRunner {
                 int inputNumber = scanner.nextInt();
                 switch (inputNumber) {
                     case 1:
-                        System.out.println("Input array length");
+                        System.out.println("Enter array length");
                         arrayLength = scanner.nextInt();
                         LoopsAndArrays.showAnArrayOfEvenNumbers(arrayLength);
                         break;
                     case 2:
-                        System.out.println("Input array length");
+                        System.out.println("Enter array length");
                         arrayLength = scanner.nextInt();
                         LoopsAndArrays.showAnArrayOfOddNumbers(arrayLength);
                         break;
                     case 3:
-                        System.out.println("Input array length");
+                        System.out.println("Enter array length");
                         arrayLength = scanner.nextInt();
-                        System.out.println("Input min");
+                        System.out.println("Enter the minimum boundary");
                         min = scanner.nextInt();
-                        System.out.println("Input max");
+                        System.out.println("Enter the maximum boundary");
                         max = scanner.nextInt();
-                        LoopsAndArrays.showAnArrayOfRandomNumbersAndCalculateHowManyEven(arrayLength, min, max);
+                        LoopsAndArrays.showHowManyEvenNumbers(arrayLength, min, max);
                         break;
                     case 4:
-                        System.out.println("Input array length");
+                        System.out.println("Enter array length");
                         arrayLength = scanner.nextInt();
-                        System.out.println("Input min");
+                        System.out.println("Enter the minimum boundary");
                         min = scanner.nextInt();
-                        System.out.println("Input max");
+                        System.out.println("Enter the maximum boundary");
                         max = scanner.nextInt();
-                        LoopsAndArrays.showAnArrayOfRandomNumbersAndDefineMinNumber(arrayLength,min,max);
+                        LoopsAndArrays.showMinNumber(arrayLength, min, max);
                         break;
                     case 5:
-                        System.out.println("Input array length");
+                        System.out.println("Enter array length");
                         arrayLength = scanner.nextInt();
-                        System.out.println("Input min");
+                        System.out.println("Enter minimum boundary");
                         min = scanner.nextInt();
-                        System.out.println("Input max");
+                        System.out.println("Enter maximum boundary");
                         max = scanner.nextInt();
-                        LoopsAndArrays.showAnArrayOfRandomNumbersAndDefineMaxNumber(arrayLength, min, max);
+                        LoopsAndArrays.showMaxNumber(arrayLength, min, max);
                         break;
                     case 6:
-                        System.out.println("Input array length");
+                        System.out.println("Enter length");
                         arrayLength = scanner.nextInt();
-                        System.out.println("Input array length");
+                        System.out.println("Enter length 2");
                         arrayLength2 = scanner.nextInt();
-                        System.out.println("Input min");
+                        System.out.println("Enter minimum boundary");
                         min = scanner.nextInt();
-                        System.out.println("Input max");
+                        System.out.println("Enter maximum boundary");
                         max = scanner.nextInt();
                         LoopsAndArrays.showMultidimensionalArrayOfRandomNumbers(arrayLength, arrayLength2, min, max);
                     case 7:
@@ -86,120 +87,3 @@ public class Lesson7LoopsArraysRunner {
         }
     }
 }
-
-
-   /* public static void showAnArrayOfEvenNumbers() {
-
-        int[] array = new int[11];
-        for (int i = 1; i < array.length; i++) {
-            array[i] = i * 2;
-        }
-        for (int i = 1; i < array.length; i++) {
-            System.out.print(array[i] + " "); //  массив в строку
-        }
-        System.out.println(" ");
-        System.out.println("");
-        for (int i = 1; i < array.length; i++) {
-            System.out.println(array[i] + " "); // массив в столбик
-        }
-    } */
-
-   /* public static void showAnArrayOfOddNumbers() {
-
-        int[] array = new int[50];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = i * 2 + 1;
-        }
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " "); //  массив в строку
-        }
-        System.out.println(" ");
-        System.out.println("");
-        for (int i = 49; i >= 0; i--) {
-            System.out.println(array[i] + " "); // массив в столбик
-        }
-    } */
-
-   /* public static void showAnArrayOfNumbers() {
-
-        int evenNumbers = 0;
-
-        int[] array = new int[16];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = (int) (Math.random() * 10); // генерация случайных чисел
-        }
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " "); //  массив в строку
-        }
-
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 == 0) {
-                evenNumbers++;
-            }
-        }
-        System.out.println(" ");
-        System.out.println(" ");
-        System.out.println("There are " + evenNumbers + " even numbers"); // вывести на экран количество четных чисел
-    } */
-
-    /*public static void showAnArrayOfNumbers2() {
-
-        int[] array = new int[16];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = (int) (Math.random() * 1000); // генерация случайных чисел
-        }
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " "); //  массив в строку
-        }
-
-        System.out.println(" ");
-        System.out.println(" ");
-
-        int min = array[0]; // пусть минимум у нас будет 1-элемент в массиве
-        int max = array[0]; // пусть максимум у нас будет 1-элемент в массиве
-
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] < min) {
-                min = array[i];
-            }
-            if (array[i] > max) {
-                max = array[i];
-            }
-        }
-        System.out.println("Minimum number is " + min);
-        System.out.println("Maximum number is " + max);
-    } */
-
-   /* public static void showAnArrayOfMultidimensionalNumbers() {
-
-        int[][] array = new int[8][5];
-        for (int i = 0; i < array.length; i++) {
-            for (int k = 0; k < array[i].length; k++) {
-                array[i][k] = (int) (Math.random() * 100); // генерация случайных чисел
-            }
-        }
-
-        for (int i = 0; i < array.length; i++) {
-            for (int k = 0; k < array[i].length; k++) {
-                System.out.print(array[i][k] + " ");  //  массив в строку
-            }
-        }
-    } */
-
-   /* public static void showAnArrayOfMultidimensionalNumbers2() {
-
-        int[][] array = new int[8][5];
-        for (int i = 0; i < array.length; i++) {
-            for (int k = 0; k < array[i].length; k++) {
-                array[i][k] = (int) (Math.random() * 1000); // генерация случайных чисел
-            }
-        }
-
-        for (int i = 0; i < array.length; i++) {
-            System.out.printf("%d%s", i + 1, ":"); // %s - отобразить, как String; %d отобразить, как int;
-            for (int k = 0; k < array[i].length; k++) {
-                System.out.printf("%5d", array[i][k]); // отступ в 5 символов между числами в столбцах
-            }
-            System.out.println(""); // отступ между строк
-        }
-    } */
